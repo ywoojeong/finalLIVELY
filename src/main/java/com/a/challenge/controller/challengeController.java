ba1fc7381594e39235de93cb22446e56aa00e62f
bitca <bitca@DESKTOP-QAGJB3V>
2021-05-12 18:17:18 +0900

    05.12 18:17 민선 와장창창

---
+++ src/main/java/com/a/challenge/controller/challengeController.java
@@ -0,0 +1,28 @@
+package com.a.challenge.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.a.challenge.service.challengeService;
+
+@Controller
+public class challengeController {
+	
+	@Autowired
+	challengeService service;
+	
+	@RequestMapping(value = "hotChallenge.do", method = {RequestMethod.GET, RequestMethod.POST})
+	public String hotChallenge() {
+		
+		return "challenge/newChallenge";
+	}
+	
+	@RequestMapping(value = "newChallenge.do", method = {RequestMethod.GET, RequestMethod.POST})
+	public String newChallenge() {
+		
+		return "challenge/newChallenge";
+	}
+	
+}
