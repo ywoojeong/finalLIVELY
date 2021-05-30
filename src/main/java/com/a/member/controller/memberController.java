1b8aaadd1f41f750ae30b06fddbef7ec8b093781
ywoojeong <ywoo0309@gmail.com>
2021-05-30 13:54:01 +0900

    20210530_오전 커밋

--- src/main/java/com/a/member/controller/memberController.java
+++ src/main/java/com/a/member/controller/memberController.java
@@ -107,4 +107,48 @@ public class memberController {
 		return "redirect:intro.do";
 	}
 	
+	/*
+	 정연우
+	 네이버 로그인 이동
+	*/
+	@RequestMapping(value="memberNaverLoginPro.do",  method = {RequestMethod.GET,RequestMethod.POST})
+	public String memberNaverLoginPro(Model model, MemberDto dto, @RequestParam Map<String,Object> paramMap, @RequestParam String code, @RequestParam String state,HttpSession session) throws SQLException, Exception {
+		System.out.println("멤버 네이버 로그인 프로 paramMap:" + paramMap);
+//		Map <String, Object> resultMap = new HashMap<String, Object>();
+		System.out.println("memberNaverLoginPro.do 실행");
+		OAuth2AccessToken oauthToken;
+		oauthToken = naverloginbo.getAccessToken(session, code, state);
+		//로그인 사용자 정보를 읽어온다
+		String apiResult = naverloginbo.getUserProfile(oauthToken);
+		System.out.println("memberNaverLoginPro 값 apiResult =>"+apiResult);
+		ObjectMapper objectMapper =new ObjectMapper();
+		Map<String, Object> apiJson = (Map<String, Object>) objectMapper.readValue(apiResult, Map.class).get("response");
+		apiJson.put("naverLogin", apiJson.get("id"));
+		//apiJson.put("password",apiJson.get("id"));
+		Map<String, Object> naverConnectionCheck = service.naverConnectionCheck(apiJson);
+		
+		//naverConnectionCheck가 null이면 첫 회원가입
+		if(naverConnectionCheck == null) { //일치하는 이메일 없으면 가입
+			// 뷰단으로 로그인 정보 보내주기 (memberInfo.jsp)
+			model.addAttribute("email",apiJson.get("email"));
+			model.addAttribute("naverLogin",apiJson.get("id"));
+			model.addAttribute("flag","naver");
+			return "member/memberInfo";
+			
+		// NAVERLOGIN이 null이지만 이메일 정보가 있을때 -> 네이버 연동 처리
+		}else if(naverConnectionCheck.get("NAVERLOGIN") == null && naverConnectionCheck.get("EMAIL") != null) { //이메일 가입 되어있고 네이버 연동 안되어 있을시
+			service.setNaverConnection(apiJson);
+			MemberDto loginCheck = service.memberNaverLoginPro(apiJson);
+			session.setAttribute("memberInfo", loginCheck);
+			System.out.println("네이버로그인 1 세션 : " + session);
+			System.out.println("네이버로그인 1 로그인체크 : " + loginCheck);
+		}else { //이미 연동이 되어있고 가입이 되어있을 때
+			MemberDto loginCheck = service.memberNaverLoginPro(apiJson);
+			session.setAttribute("memberInfo", loginCheck);
+			System.out.println("네이버로그인 2 세션 : " + session);
+			System.out.println("네이버로그인 2 로그인체크 : " + loginCheck);
+		}
+
+		return "redirect:memberCon.do";
+	}
 }
