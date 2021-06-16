78eed80dba106aebaca0b4d9d88cc7ee5d42ea4e
LemonLimeL <dlalstjs0226@naver.com>
2021-06-17 00:52:15 +0900

    06.17 00:52 집 작업 마감

--- src/main/java/com/a/manager/dao/managerDaoImpl.java
+++ src/main/java/com/a/manager/dao/managerDaoImpl.java
@@ -14,10 +14,65 @@ public class managerDaoImpl implements managerDao{
 	@Autowired
 	SqlSession managerSql;
 	
+	String ns = "manager.";
+	
 	@Override
 	public List<Map<String, Object>> managerList(Map<String, Object> mListParam) throws SQLException {
 		// TODO Auto-generated method stub
 		System.out.println("mListParam===========>"+mListParam);
 		return managerSql.selectList("manager.managerList", mListParam);
 	}
+
+	
+	
+	//민선 부분
+	@Override
+	public int userAllCount() {
+		// TODO Auto-generated method stub
+		return managerSql.selectOne(ns+"userAllCount");
+	}
+
+	@Override
+	public int challengeAllCount() {
+		// TODO Auto-generated method stub
+		return managerSql.selectOne(ns+"challengeAllCount");
+	}
+
+	@Override
+	public List<Map<String, Object>> challengeAdd() {
+		// TODO Auto-generated method stub
+		return managerSql.selectList(ns+"challengeAdd");
+	}
+
+	@Override
+	public List<Map<String, Object>> challengeCategory() {
+		// TODO Auto-generated method stub
+		return managerSql.selectList(ns+"challengeCategory");
+	}
+
+	@Override
+	public List<Map<String, Object>> challengeMemberMax() {
+		// TODO Auto-generated method stub
+		return managerSql.selectList(ns+"challengeMemberMax");
+	}
+
+	@Override
+	public List<Map<String, Object>> userAdd() {
+		// TODO Auto-generated method stub
+		return managerSql.selectList(ns+"userAdd");
+	}
+
+	@Override
+	public List<Map<String, Object>> userFollowingMax() {
+		// TODO Auto-generated method stub
+		return managerSql.selectList(ns+"userFollowingMax");
+	}
+
+	@Override
+	public List<Map<String, Object>> userPointMax() {
+		// TODO Auto-generated method stub
+		return managerSql.selectList(ns+"userPointMax");
+	}
+
+	
 }
--- src/main/java/com/a/manager/service/managerService.java
+++ src/main/java/com/a/manager/service/managerService.java
@@ -7,4 +7,28 @@ import java.util.Map;
 public interface managerService {
 	
 	public List<Map<String, Object>> managerList(Map<String, Object> mListParam) throws SQLException;
+	
+	
+	//민선 부분
+	
+	//전체 사용자 수 userAllCount + 전체 챌린지 수 challengeAllCount
+	public Map<String, Object> allCount();
+	
+	//월별 챌린지 생성 개수 challengeAdd
+	public Map<String, Object> challengeAdd();
+	
+	//카테고리별 챌린지 개수  challengeCategory
+	public Map<String, Object> challengeCategory();
+	
+	//챌린지 가입인원 수 challengeMemberMax
+	public List<Map<String, Object>> challengeMemberMax();
+	
+	//월별 사용자 가입수 userAdd
+	public Map<String, Object> userAdd();
+	
+	//좋아요 많이 받은 순(following) userFollowingMax
+	public List<Map<String, Object>> userFollowingMax();
+	
+	//포인트가 많은 사용자 순 userPointMax
+	public List<Map<String, Object>> userPointMax();
 }
